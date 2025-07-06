943a94e80aa60f83d573769ec051a97df007a24a
Igor Moscaliov <maskalfm@gmail.com>
2025-07-06 18:06:37 +0300

    the main entities were created

---
+++ DevQuestions/src/DevQuestion.Domain/Comments/Comment.cs
@@ -0,0 +1,17 @@
+﻿namespace DevQuestion.Domain.Comments;
+
+public class Comment
+{
+    public Guid Id { get; set; }
+    
+    public Guid UserId { get; set; }
+    
+    public Comment? Parent { get; set; }
+    
+    public required Guid EntityId { get; set; } // это сущность для которой оставили комментарий (вопрос, ответ, видео, коммент...)
+    
+    // когда в логике нам нужен тип того, к чему делаем комментарий 
+    // public string EntityType { get; set; } // чтобы понимать под какой сущностью мы оставили комментарий (вопрос, ответ, видео, коммент...)
+
+    public List<Comment> Children { get; set; } = [];
+}
---
+++ DevQuestions/src/DevQuestion.Domain/Questions/Question.cs
@@ -0,0 +1,24 @@
+﻿namespace DevQuestion.Domain.Questions;
+
+public class Question
+{
+    public Guid Id { get; set; } // ид вопроса 
+    
+    public DateTime CreatedAt { get; set; } // дата создания
+    
+    public required string Title { get; set; } 
+    
+    public required string Text { get; set; } 
+    
+    public Guid? ScreesShotId { get; set; } // что то важное 
+    
+    public required Guid UserId { get; set; } //  ид того кто оставил вопрос  
+    
+    public List<Answer> Answers { get; set; } = []; // список ответов
+    
+    public Answer? Solution { get; set; } // знак ? после имени типа (Answer?) означает, что свойство может быть null
+    
+    public List<Guid> Tags { get; set; } = [];
+    
+    // public List<Guid> Comments { get; set; } = []; // если большой сервис - может потребовать много ресурсов.
+}
